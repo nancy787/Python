6f502939d03765363a4eebeeda9eebeb079a6e55
Nancy <nancykumari2630@gmail.com>
2026-04-30 09:02:11 +0530

    Add recursive functions for printing names, numbers, sum, factorial, reversing arrays, and Fibonacci sequence

---
+++ Recursion/print_name.py
@@ -0,0 +1,71 @@
+def print_name(n) :
+    if (n == 0) :
+        return 
+    
+    print('nancy')
+    return print_name( n - 1 )
+
+print(print_name(3))
+
+# print 1 to n
+def print_n_to_1(num) :
+    if num == 0 :
+        return 
+    print(num)
+    return print_n_to_1(num - 1)
+
+print(print_n_to_1(4))
+
+def printnum(curr, num) :
+    if curr > num :
+        return
+    
+    print(curr)
+    printnum(curr + 1, num)
+    return 
+
+printnum(1, 4)
+
+# Sum of n natural number
+def sumofNum(n) :
+    if n == 1 : 
+        return 1
+    
+    return n + sumofNum(n - 1)
+    
+print(sumofNum(5))
+
+# Factorial of a number
+def factorial(n) : 
+    if n == 0 :
+        return 1
+    
+    return n * factorial( n - 1)
+
+print(factorial(5))
+
+def reverse_array(arr) : 
+    p1 = 0
+    p2 = len(arr) - 1
+    while p1 < p2 :
+        arr[p1], arr[p2] = arr[p2], arr[p1]
+        p1 += 1
+        p2 -= 1
+
+    return arr
+
+num = [5,4,3,2,1]
+print(reverse_array(num))
+
+# Fibbonacci 
+def fibbo(n) :
+    if n <= 1 :
+        return n
+    
+    last = fibbo(n - 1)
+    slast = fibbo(n - 2)
+
+    return last + slast
+
+fibbo(5)
+
