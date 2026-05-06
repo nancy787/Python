a96bcca7745a98510829c4c3b190a89ef27b7e64
Nancy <nancykumari2630@gmail.com>
2026-05-06 08:22:14 +0530

    largest and second largest

---
+++ lists/practice-ques.py
@@ -0,0 +1,29 @@
+# Find the Largest element in an array
+def largest(arr) :
+    max = arr[0]
+    for i in range(0, len(arr)) :
+        if arr[i] > max :
+            max = arr[i]
+    return max
+
+print(largest([8, 10, 5, 7, 9]))
+# time complexity  - o(n)
+# space complexity = o(n)
+
+#Find Second Smallest and Second Largest Element in an array
+def secondLargest(arr) :
+    max = arr[0]
+    secondLarge = -1
+    for i in range(1, len(arr)) :
+        if secondLarge > max :
+            secondLarge = max
+            max = arr[i]
+        elif arr[i] > secondLarge and arr[i]!=  max:
+            secondLarge = arr[i]
+
+    return secondLarge
+
+print(secondLargest([1, 2, 4, 7, 7, 5]))
+# time complesity o(n)
+# space complecity o(n)
+
