1c85aa51feaf4e7720e41b0df053a09d44ddf7b5
Nancy <nancykumari2630@gmail.com>
2026-05-09 09:50:58 +0530

    add function to determine highest and lowest frequency in a list

--- Hashing/hash.py
+++ Hashing/hash.py
@@ -13,4 +13,34 @@ def countFrequency(nums) :
 nums = [10,5,10,15,10,5]
 print(countFrequency(nums))
 
-# highest orders
+# highest and lowest freqency
+
+def HighandLowFrequency(nums) :
+    freq = {} 
+    for val in nums :
+        if val in freq :
+            freq[val] += 1
+        else :
+            freq[val] = 1
+
+    highest_element = None
+    highest_count = 0
+
+    lowest_element = None
+    lowest_count = float('inf')
+
+    freq = HighandLowFrequency(nums)
+
+    for key, count in freq.items():
+        if count > highest_count:
+            highest_count = count
+            highest_element = key
+        if count < lowest_count:
+            lowest_count = count
+            lowest_element = key
+
+
+    print("Highest:", highest_element, "->", highest_count)
+    print("Lowest:", lowest_element, "->", lowest_count)
+
+    print(HighandLowFrequency([10,5,10,15,10,5]))
