76d85907cf7eedf16c561cee67e939e53a0f30bf
Nancy <nancykumari2630@gmail.com>
2026-05-09 09:23:28 +0530

    refactor countFrequency function to improve frequency counting logic

--- Hashing/hash.py
+++ Hashing/hash.py
@@ -1,10 +1,16 @@
 # Count Frequincy in list
 # hasing = presetore and fetch
-mylist = [10,5,10,15,10,5]
-number =5
-count = 0
-for i in mylist :
-    if(mylist[i] == number) :
-        count += 1
+def countFrequency(nums) :
+    freq = {}
+    for val in nums :
+        if val in freq :
+            freq[val] += 1
+        else :
+            freq[val] = 1
 
-print(count)
+    return freq
+
+nums = [10,5,10,15,10,5]
+print(countFrequency(nums))
+
+# highest orders
