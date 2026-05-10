1e1c832f2248ab2da6fb364f51c460eacd7db447
Nancy <nancykumari2630@gmail.com>
2026-05-10 22:15:10 +0530

    add function to left rotate an array by one

--- lists/practice-ques.py
+++ lists/practice-ques.py
@@ -72,3 +72,16 @@ k = removeDuplicatesTwoPointers(nums)
 
 print(k)
 print(nums[:k])
+
+# Left Rotate the Array by One
+
+def leftRotate(nums) :
+    temp = nums[0]
+    for i in range(1, len(nums)) : 
+        nums[i - 1] = nums[i]
+
+    nums[len(nums) - 1] = temp
+
+    return nums
+rotate = leftRotate([1, 2, 3, 4, 5])
+print(rotate)
