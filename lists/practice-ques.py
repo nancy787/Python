a394a369d95fc612fe28bd89a12d27f94577734d
Nancy <nancykumari2630@gmail.com>
2026-05-14 09:11:09 +0530

    find subarray

--- lists/practice-ques.py
+++ lists/practice-ques.py
@@ -268,4 +268,14 @@ def getSingleElem(nums) :
 se = getSingleElem([4,1,2,1,2])
 print("se", se)
 
+# FIND SUBARRAY
 
+def subarray(arr) :
+    n = len(arr)
+    for i in range(0, n) :
+        for j in range(i, n) :  
+            for k in range(i, j + 1) :
+                print(arr[k], end=" ")
+        print()
+subarr = subarray([10,5,2,7,1]) 
+print(subarr)
