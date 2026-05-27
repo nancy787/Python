a681578a3f9314a9e621d24615cde43f56fa051c
Nancy <nancykumari2630@gmail.com>
2026-05-27 08:05:51 +0530

    Add Recursive Binary Search implementation in bs.py

--- BinarySearch/bs.py
+++ BinarySearch/bs.py
@@ -18,3 +18,28 @@ def BinarySearch(nums, target) :
 nums = [2,4,6,7,9,12,16,17]
 binary_search = BinarySearch(nums, 9)
 print(binary_search)
+
+
+def RecursiveBinarySearch(nums, low, high, target) :
+    if low > high :
+        return - 1
+    mid = (low + high )//2
+
+    if nums[mid] == target :
+        return mid
+    elif target > nums[mid] :
+         return RecursiveBinarySearch(nums, mid + 1, high, target)
+    else :
+       return RecursiveBinarySearch(nums, low, mid - 1, target)
+
+
+nums = [2,4,6,7,9,12,16,17]
+low = 0
+high = len(nums) - 1
+search = RecursiveBinarySearch(nums, low, high, 9)
+print(search)
+
+# Time complexity 
+# O(log2n)
+
+
