90bb18ab29f4e936bdb3d6fef9d09828362ba0f2
Nancy <nancykumari2630@gmail.com>
2026-06-02 07:51:57 +0530

    nth square root

--- BinarySearch/math.py
+++ BinarySearch/math.py
@@ -29,3 +29,25 @@ def SquareRoot(n) :
 square_root = SquareRoot(36) 
 print('square_root', square_root)
 
+# Nth Root of a Number using Binary Search
+def nthRoot(n, m) :
+    low = 1
+    high = m
+
+    while low <= high :
+        mid = (low + high) //2
+        value = mid ** n
+        if value == m :
+            return mid
+        elif value < m :
+            low = mid + 1
+
+        else :
+            high = mid - 1
+
+    return -1
+
+nth_rooot = nthRoot(3, 27) 
+print('nth_rooot', nth_rooot)
+# Time Complexity: O(log m)
+# Space Complexity: O(1)
