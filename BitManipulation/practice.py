b830e34ce382667ffcb4c7f99cfde7c4485b9dd9
Nancy <nancykumari2630@gmail.com>
2026-06-17 09:00:29 +0530

    Add function to convert binary to decimal

--- BitManipulation/practice.py
+++ BitManipulation/practice.py
@@ -14,3 +14,19 @@ print('decimal to binary' , decmtobinary)
 # Time compleixty O(Log2n)
 # Space  compleixty O(Log2n)
 
+def BinaryToDecimal(str) :
+    p2 = 1
+    num = 0
+
+    for i in range(len(str) - 1, -1, -1) :
+        if str[i] == '1' :
+            num = num + p2
+        p2 = p2 * 2
+    
+    return num
+
+str = '1101'
+decimal_to_binary = BinaryToDecimal(str)
+print('decimal to binary' , decimal_to_binary)
+# Time complxity O(len)
+# Time complxity O(1)
